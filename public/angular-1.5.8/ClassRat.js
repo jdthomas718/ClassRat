f1d77f3f0c705068c69d2c7d0ea15ce98daa2d45
Justin Soderstrom <jasoderstrom1@gmail.com>
2016-10-18 02:10:46 +0000

    Added super basic angular functionality to show class listing

--- public/angular-1.5.8/ClassRat.js
+++ public/angular-1.5.8/ClassRat.js
@@ -1,2 +1,18 @@
+//Hard-coded class data
+var classListCtrl = function($scope) {
+    $scope.data = {
+        classes: [{
+            id: 0xf00b412,
+            code: "CSC 150",
+            name: "Computer Science 1"
+        }, {
+            id: 0xf00b413,
+            code: "CSC 250",
+            name: "Computer Science 2"
+        }]
+    };
+}
+
 angular
-    .module('ClassRat', []);
+    .module('ClassRat', [])
+    .controller('classListCtrl', classListCtrl);
