a2f31269d955157fcf297fef983e81649cb6c467
Jacob Thomas <jacob.d.thomas718@gmail.com>
2016-10-28 18:25:50 +0000

    Updated favicon to favicon.ico

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
