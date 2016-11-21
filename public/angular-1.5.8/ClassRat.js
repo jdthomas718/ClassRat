80c50d2336e32cdb0613901af17108c97757704b
Jacob Thomas <jacob.d.thomas718@gmail.com>
2016-11-21 22:42:30 +0000

    Final commits

--- public/angular-1.5.8/ClassRat.js
+++ public/angular-1.5.8/ClassRat.js
@@ -1,54 +1,81 @@
 /******** Controllers ***********************/
-var classCtrl = function($scope, sharedReviewInputFormService) {
-    sharedReviewInputFormService.setReviewsLoaded(false);
+var classCtrl = function($scope, $http, sharedReviewInputFormService, getClassDataService, getClassSectionService, getClassReviewService) {
+    $scope.sharedObj = sharedReviewInputFormService;
     $scope.sections = [];
     $scope.sectionReviews = [];
-    $scope.classId = {};
+    $scope.classId = "";
     $scope.className = "";
-    $scope.classListings = [
-        {
-            id: 0xf00b412,
-            code: "CSC 150",
-            name: "Computer Science 1"
-        }, 
-        {
-            id: 0xf00b413,
-            code: "CSC 250",
-            name: "Computer Science 2"
-        }, 
-        {
-           id: 0xf00b414,
-           code: "MATH 201",
-           name: "Intro to Discrete Math"
-        }
-         ];
+    $scope.classListings = [];
+    $scope.sharedObj.setReviewsLoaded(false);
 
+    $scope.getClassData = function() {
+        // Calls the async method defined in the service that returns a promise
+        // The then method of the promise is used to set $scope.classListings to the data response
+        getClassDataService.async().then(function(data) {
+            $scope.classListings = data;
+        });
+    };
+    
     // will use class id to retrieve and populate the list of class sections
-	$scope.getClassSections = function(className) {
-		$scope.sections = classSectionService(className);
+	$scope.getClassSections = function(classId, className) {
+	    // Calls the async method defined in the service that returns a promise
+        // The then method of the promise is used to set $scope.sections to the data response
+		getClassSectionService.async(classId).then(function(sections) {
+		    $scope.sections = sections;
+		});
 		$scope.className = className;
+		$scope.classId = classId;
+		$scope.sharedObj.setSharedClassId(classId);
 	};
 	
 	$scope.getSectionReviews = function(profId) {
-	    $scope.sectionReviews = classReviewService(profId);
-	    sharedReviewInputFormService.setReviewsLoaded(true);
+	    // Calls the async method defined in the service that returns a promise
+        // The then method of the promise is used to set $scope.sectionReviews to the data response
+	    getClassReviewService.async(profId, $scope.classId).then(function(reviews) {
+	        $scope.sectionReviews = reviews;
+	    });
+	    $scope.sharedObj.setReviewsLoaded(true);
+	    $scope.sharedObj.setSharedProfId(profId);
+	    $scope.sharedObj.submitted = false;
 	};
 	
 	$scope.clearSearchInfo = function() {
 	    $scope.search = "";
 	    $scope.sectionReviews = [];
-	    sharedReviewInputFormService.setReviewsLoaded(false);
+	    $scope.sharedObj.setReviewsLoaded(false);
 	};
 	
 };
 
-var reviewInputCtrl = function($scope, sharedReviewInputFormService) {
-    $scope.reviewsLoaded = sharedReviewInputFormService;
-    //alert($scope.reviewsLoaded);
+var reviewInputCtrl = function($scope, $http, sharedReviewInputFormService, addReviewService) {
+    $scope.sharedObj = sharedReviewInputFormService;
+    $scope.newReview = {}; //newReview object that will hold the newly created review
+    $scope.newReview.username = "Anonymous"; //Default Username
+    $scope.newReview.rating = 3; //Default rating
+    $scope.newReview.text = "";
+    $scope.onSubmit = function() {
+        $scope.newReview.profId = $scope.sharedObj.getSharedProfId();
+        $scope.newReview.classId = $scope.sharedObj.getSharedClassId();
+        addReviewService.async($scope.newReview).then(function() {
+            $scope.newReview.username = "Anonymous";
+            $scope.newReview.rating = 3;
+            $scope.newReview.text = "";
+          //  alert("Thanks!");
+        });
+        $scope.sharedObj.submitted = true;
+        /*alert($scope.newReview.profId);
+        alert($scope.newReview.classId);
+        alert($scope.newReview.username);
+        alert($scope.newReview.rating);
+        alert($scope.newReview.text);*/
+    };
 };
+
 /******** End Controllers ********************/
 
 /******** Directives *************************/
+
+// Directive for the class-section buttons
 var classButtonItem = function() {
 	return {
 	    restrict: 'A',
@@ -58,6 +85,7 @@ var classButtonItem = function() {
 	};
 };
 
+// Directive for displaying reviews
 var review = function() {
     return {
         restrict: 'A',
@@ -73,57 +101,124 @@ var review = function() {
     };
 };
 
-var reviewInputForm = function() {
+// Directive for the review input form
+var reviewInputForm = function($compile) {
     return {
         restrict: 'A',
         replace: false,
         templateUrl: '/angular-1.5.8/writeReview.html',
-        scope: {
-            display: '='
-        }
-    };
+        scope: true
+    }
+};
+
+//Star Rating Directive for User Input Form
+var starRating = function() {
+  return {
+		restrict : 'A',
+		templateUrl : '/angular-1.5.8/stars.html',
+		scope : {
+			ratingValue : '=ngModel',
+			max : '='
+		},
+		link : function(scope, elem, attrs) {
+			var updateStars = function() {
+				scope.stars = [];
+				for ( var i = 0; i < scope.max; i++) {
+					scope.stars.push({
+						filled : i < scope.ratingValue
+					});
+				}
+			};
+			
+			scope.toggle = function(index) {
+				scope.ratingValue = index + 1;
+			};
+			
+			scope.$watch('ratingValue',
+				function(oldVal, newVal) {
+					if (newVal) {
+						updateStars();
+					}
+				}
+			);
+		}
+	};
 };
 /**** End Directives ***********************/
 
 /**** Services *****************************/
-var classSectionService = function(className) {
-    return  [   {
-                    _id: 0x1234,
-            		name: "Tom Halverson",
-            		average: 3.5,
-            		ratings: [3, 4]
-                },
-            	{
-            		_id: 0x1235,
-            		name: "Jason Jenkins",
-            		average: 4.5,
-            		ratings: [5, 5, 4, 4]
-                }
-            ];
-        
+
+//Returns a promise object returned by $http service for the initial class data API call 
+var getClassDataService = function($http) {
+    var ClassDataService = {
+        // $http returns a promise, which has a then function, which also returns a promise
+        async: function() {
+            var promise = $http.get('https://brosz-class-rat-nbrosz.c9users.io/api/classes/').then(function(response) {
+        // The return value gets picked up by the then method in the controller.
+                return response.data;
+            },
+            //On failure
+            function(error)
+            {
+                console.log("ClassDataService Request Failed: " + error);
+            });
+        // Return the promise to the controller
+            return promise;
+        }
+    };
+    return ClassDataService;
 };
 
-var classReviewService = function(profId) {
-    return [
-        	{
-        		id: "580ff27d49e785f8d1e4c678",
-        		created: "05/17/2016",
-        		username: "anonymous",
-        		rating: 4,
-        		text: "Review goes here."
-        	},
-        	{
-        		id: "580ff28749e785f8d1e4c679",
-        		created: "07/18/2015",
-        		username: "anonymous",
-        		rating: 3,
-        		text: ""
-        	}
-        ];
+//Returns a promise object returned by $http service for the class section API call
+//This service relies on the unique class id of the class chosen by the user to make the call
+var getClassSectionService = function($http) {
+    var ClassSectionService = {
+        // $http returns a promise, which has a then function, which also returns a promise
+        async: function(classId) {
+            var promise = $http.get('https://brosz-class-rat-nbrosz.c9users.io/api/classes/' + classId + '/professors/').then(function(response) {
+        // The return value gets picked up by the then method in the controller.
+                return response.data;
+            },
+            //On failure
+            function(error)
+            {
+                console.log("ClassSectionService Request Failed: " + error);
+            });
+        // Return the promise to the controller    
+            return promise;
+        }
+    }; 
+    return ClassSectionService;
+};
+
+//Returns a promise object returned by $http service for the class section reviews API call
+//This service relies on the unique class id and professor id of the class and professor chosen by the user to make the call
+var getClassReviewService = function($http) {
+    var ClassReviewService = {
+        // $http returns a promise, which has a then function, which also returns a promise    
+        async: function(profId, classId) {
+            var promise = $http.get('https://brosz-class-rat-nbrosz.c9users.io/api/classes/' + classId + '/professors/' + profId + '/reviews/' ).then(function(response) {
+        // The return value gets picked up by the then method in the controller.
+                return response.data;
+            },
+            //On failure
+            function(error)
+            {
+                console.log("ClassReviewService Request Failed: " + error);
+            });
+        // Return the promise to the controller    
+            return promise;
+        }
+    };
+    
+    return ClassReviewService;
 };
 
 var sharedReviewInputFormService = function() {
         this.loaded = false;
+        this.profId = "";
+        this.classId = "";
+        this.submitted = false;
         
         this.getReviewsLoaded = function() {
             return this.loaded;
@@ -132,7 +227,56 @@ var sharedReviewInputFormService = function() {
         this.setReviewsLoaded = function(reviewsLoaded) {
             this.loaded = reviewsLoaded;
         };
+        
+        this.setSharedClassId = function(inputClassId) {
+            this.classId = inputClassId;
+        }
+        
+        this.getSharedClassId = function() {
+            return this.classId;
+        }
+        
+        this.setSharedProfId = function(inputProfId) {
+            this.profId = inputProfId;
+        }
+        
+        this.getSharedProfId = function () {
+            return this.profId;
+        }
 };
+
+
+var addReviewService = function($http) {
+
+    var reviewService = {
+            //$http returns a promise, which has a then function, which also returns a promise
+            async: function(newReview) {
+                
+                //Create new data object for POST request
+                var review = {};
+                review.username = newReview.username;
+                review.rating = newReview.rating;
+                review.text = newReview.text;
+                
+                var promise = $http.post('https://brosz-class-rat-nbrosz.c9users.io/api/classes/'+newReview.classId+'/professors/'+newReview.profId+'/reviews/', newReview).then(
+                    //On success
+                    function(response) {
+                        alert("POST success!");
+                     },
+                     //On failure
+                    function(error) {
+                        alert("addReviewService request failed:"+JSON.stringify(error));
+                    }
+            );
+            //Return promise to the controller
+            return promise;
+        }
+    };
+    
+    return reviewService;
+};
+
+
 /**** End Services **************************/
 
 angular
@@ -142,7 +286,9 @@ angular
     .directive('classButtonItem', classButtonItem)
     .directive('review', review)
     .directive('reviewInputForm', reviewInputForm)
+    .directive('starRating', starRating)
     .service('sharedReviewInputFormService', sharedReviewInputFormService)
-    .service('classSectionService', classSectionService)
-    .service('classReviewService', classReviewService);
-    
+    .service('getClassDataService', getClassDataService)
+    .service('getClassSectionService', getClassSectionService)
+    .service('getClassReviewService', getClassReviewService)
+    .service('addReviewService', addReviewService);
